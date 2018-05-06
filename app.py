1860ee4ca9440c69b78a2ad31a13680804a113ff
Joonyoung Kim <joony0123@gmail.com>
2018-05-06 14:24:27 -0400

    changed

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def test_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
--- app_namespace.py
+++ app_namespace.py
@@ -96,4 +96,4 @@ socketio.on_namespace(MyNamespace('/test'))
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
